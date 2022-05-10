6d6bc4e5bd593999f9bfd553e06e04da3d8210e7
Mahmoud Kassah <kassah.webdev@gmail.com>
2022-05-10 20:29:27 +0300

    esri gis simple demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect } from "react";
+import MapView from "@arcgis/core/views/MapView";
+import Map from "@arcgis/core/Map";
+import esriConfig from "@arcgis/core/config";
+import Graphic from "@arcgis/core/Graphic";
+import GraphicsLayer from "@arcgis/core/layers/GraphicsLayer";
+
+import "./App.css";
+
+const API_KEY =
+  "AAPK7b5d90c80dfa41f0bcda0ecd1c2df337eqyVrIoVyvmiKq5Y5tm2yUvt4HdPQF8wJ0zHd40WUkFUdAjEq_V4rJc0GZLSfhN3";
+
+const data = [
+  { longitude: 35.5018, latitude: 33.8938, value: 20 },
+  { longitude: 35.5441, latitude: 33.8338, value: 200 },
+  { longitude: 35.3729, latitude: 33.5571, value: 1000 },
+];
+
+const getColor = (value) => {
+  if (value < 200) return [0, 255, 0];
+  if (value < 900) return [225, 255, 0];
+  return [255, 0, 0];
+};
+
+const createPoint = (data) => {
+  const point = {
+    type: "point",
+    longitude: data.longitude,
+    latitude: data.latitude,
+  };
+
+  const simpleMarkerSymbol = {
+    type: "simple-marker",
+    color: getColor(data.value),
+    outline: {
+      color: [255, 255, 255],
+      width: 1,
+    },
+  };
+
+  const attributes = {
+    name: "Air Quality",
+    description: data.value.toString(),
+  };
+
+  return new Graphic({
+    geometry: point,
+    symbol: simpleMarkerSymbol,
+    attributes: attributes,
+    popupTemplate: {
+      title: attributes.name,
+      content: attributes.description,
+    },
+  });
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const mapDiv = useRef(null);
+
+  useEffect(() => {
+    if (mapDiv.current) {
+      /**
+       * Initialize application
+       */
+      esriConfig.apiKey = API_KEY;
+
+      const map = new Map({
+        basemap: "arcgis-streets", // Basemap layer service
+      });
+
+      const graphicsLayer = new GraphicsLayer();
+      map.add(graphicsLayer);
+
+      data.forEach((p) => {
+        const pointGraphic = createPoint(p);
+        graphicsLayer.add(pointGraphic);
+      });
+
+      new MapView({
+        map: map,
+        container: mapDiv.current,
+        center: [35.5018, 33.8938], // Longitude, latitude
+        zoom: 8, // Zoom level
+      });
+    }
+  }, []);
+
+  return <div className="mapDiv" ref={mapDiv}></div>;
 }
 
 export default App;
