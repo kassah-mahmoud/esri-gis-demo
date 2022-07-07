04cd7439069577bbc2ed7156eaa3f645060335e7
Mahmoud Kassah <kassah.webdev@gmail.com>
2022-07-07 14:37:33 +0300

    add plygon to ol

--- src/OLMap.js
+++ src/OLMap.js
@@ -5,6 +5,7 @@ import TileLayer from "ol/layer/Tile";
 import OSM from "ol/source/OSM";
 import { useGeographic } from "ol/proj";
 import Point from "ol/geom/Point";
+import Polygon from "ol/geom/Polygon";
 import Feature from "ol/Feature";
 import Overlay from "ol/Overlay";
 import { Select } from "ol/interaction";
@@ -20,6 +21,17 @@ const data = [
   { longitude: 35.52935238499353, latitude: 33.90265280208343, value: 20 },
   { longitude: 35.53390141140973, latitude: 33.90008818442558, value: 200 },
   { longitude: 35.52904052562536, latitude: 33.8997410307765, value: 1000 },
+];
+
+const polygonCoords = [
+  [
+    [35.53020049486402, 33.90179777990647],
+    [35.530733110785256, 33.90169252545216],
+    [35.5304498943827, 33.90110660661506],
+    [35.529984912229224, 33.901229404309646],
+    [35.53018781353255, 33.90179427142675],
+    [35.53020049486402, 33.90179777990647],
+  ],
 ];
 
 const getColor = (value) => {
@@ -60,8 +72,13 @@ const OLMap = () => {
           })
       );
 
+      const polygon = new Feature({
+        geometry: new Polygon(polygonCoords),
+      });
+
       const vetctorSource = new VectorSource({
-        features: [...points],
+        // features: [...points, polygon],
+        features: [...points, polygon],
       });
 
       const vectorLayer = new VectorLayer({
@@ -69,16 +86,27 @@ const OLMap = () => {
         style: function (feature) {
           const value = feature.get("value");
 
-          return new Style({
-            image: new Circle({
-              radius: 12,
-              fill: new Fill({ color: getColor(value) }),
-              // stroke: new Stroke({
-              //   color: getColor(value),
-              //   width: 2,
-              // }),
+          return [
+            new Style({
+              stroke: new Stroke({
+                color: "blue",
+                width: 2,
+              }),
+              fill: new Fill({
+                color: "rgba(0, 0, 255, 0.1)",
+              }),
             }),
-          });
+            new Style({
+              image: new Circle({
+                radius: 12,
+                fill: new Fill({ color: getColor(value) }),
+                // stroke: new Stroke({
+                //   color: getColor(value),
+                //   width: 2,
+                // }),
+              }),
+            }),
+          ];
         },
       });
 
